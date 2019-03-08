d75b5ec85ee4e55303b923c1b2d5c7bc0d093253
Alex <aly@mesosphere.com>
2019-03-07 17:31:36 -0800

    Updates

--- modulescripts/install_cassandra.sh
+++ modulescripts/install_cassandra.sh
@@ -12,11 +12,3 @@ echo "**** Installing Cassandra v$1"
 echo
 dcos package install cassandra --package-version=$1 --yes
 echo
-echo To Upgrade, use the command:
-echo
-echo dcos cassandra update package-versions --name=cassandra
-echo
-echo and
-echo
-echo dcos cassandra update start --package-version="<PACKAGE_VERSION>" --name=cassandra
-echo
